767c07f022d54b2e7ecc3b44b9af0489f8c812ef
Mark Peters <mapeters101@earthlink.net>
2025-03-07 22:33:55 -0500

    Descriptions for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,16 @@
+'''
+Question 2
+Write a function that returns a string based on two input arguments:
+
+a character or string to be repeated
+the number of times the string should be repeated
+The function should be such that the number of repetitions defaults to 10 if
+it is not passed by the caller, and the default character to be repeated
+should be a negative sign (-).
+
+Call your function separator.
+
+Use a keyword-only argument for the string argument.
+'''
+
+
