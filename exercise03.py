0f96852a654b723018f73c38b16236803e30e725
Mark Peters <mapeters101@earthlink.net>
2025-03-07 22:35:46 -0500

    Descriptions for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,10 @@
+'''
+Question 3
+Write a lambda function that returns the number of unique elements in an
+iterable. This could be the number of unique characters in a string, or the
+umber of unique elements in a list, tuple, etc.
+
+If the iterable received by the lambda function is empty, then it should
+return 0.
+'''
+
