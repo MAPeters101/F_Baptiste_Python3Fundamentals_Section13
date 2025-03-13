2de508559a90806648aaf224c6ab3dd0e0d2e9a5
Mark Peters <mapeters101@earthlink.net>
2025-03-13 14:55:47 -0400

    Work on lesson112 from 2:30 to 5:15.

--- lesson112.py
+++ lesson112.py
@@ -11,5 +11,18 @@ result = say_hello()
 print(result)
 f = say_hello
 print(globals())
+print('-'*80)
+
+l1 = [1,2,3]
+print(globals())
+l2 = l1
+print(globals())
+print(l1 is l2)
+print(f is say_hello())
+print(say_hello())
+print(f())
+print(say_hello.__name__)
+print(f.__name__)
+print('='*80)
 
 
