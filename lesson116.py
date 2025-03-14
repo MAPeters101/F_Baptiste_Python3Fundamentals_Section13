0e954db62db728168d2d14124ac12c7e4546753d
Mark Peters <mapeters101@earthlink.net>
2025-03-14 17:53:06 -0400

    Work on lesson116 from 26:30 to 29:30.

--- lesson116.py
+++ lesson116.py
@@ -172,6 +172,23 @@ def process_data(data, item_sep=',', line_sep='\n'):
     return line_sep.join(row_strings)
 
 print(process_data(data))
+print('.'*80)
+
+
+data = [
+    [10, 20, 30],
+    [100, 200, 300],
+    [1000, 2000, 3000]
+]
+
+def process_row(row, item_sep):
+    return item_sep.join(str(el) for el in row)
+
+def process_data(data, item_sep=',', line_sep='\n'):
+    row_strings = [process_row(row, item_sep) for row in data]
+    return line_sep.join(row_strings)
+
+print(process_data(data))
 print('-'*80)
 
 
