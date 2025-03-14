261626dec34ca02a852745c3179955126185215a
Mark Peters <mapeters101@earthlink.net>
2025-03-14 00:53:36 -0400

    Work on lesson114 from 6:00 to 7:35.

--- lesson114.py
+++ lesson114.py
@@ -41,7 +41,38 @@ print('-'*80)
 # my_func(1,2,3,4,5)
 # print('-'*80)
 
+print(sum({1,2,3}))
 
+def averages(*values):
+    return sum(values) / len(values)
 
+print(averages(1))
+print(averages(1,2,3))
+print(averages(1,3,5,7,9,11))
+print(averages(0))
+print('+'*80)
+
+def averages(*values):
+    try:
+        return sum(values) / len(values)
+    except ZeroDivisionError:
+        return 0
+
+print(averages(1))
+print(averages(1,2,3))
+print(averages(1,3,5,7,9,11))
+print(averages(0))
+print('-'*80)
+
+def averages(*values):
+    if len(values) == 0:
+        return 0
+    return sum(values) / len(values)
+
+print(averages(1))
+print(averages(1,2,3))
+print(averages(1,3,5,7,9,11))
+print(averages(0))
+print('.'*80)
 
 
