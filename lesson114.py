5c23f655c4a1132db984d7af23b0b17fb38a8f0c
Mark Peters <mapeters101@earthlink.net>
2025-03-14 00:45:32 -0400

    Work on lesson114 from 1:15 to 4:00.

--- lesson114.py
+++ lesson114.py
@@ -8,6 +8,28 @@ my_func(1)
 my_func(1,2,3,4, [1,2], 'abc')
 print('-'*80)
 
+def my_func(a, b, *args):
+    print(a)
+    print(b)
+    print(args)
+
+my_func(1,2)
+my_func(1,2,3,4,5)
+print('='*80)
+
+
+def my_func(a, b, *args, c):
+    print(a)
+    print(b)
+    print(args)
+    print(c)
+
+#my_func(1,2)
+#my_func(1,2,3,4,5)
+my_func(1,2,3,4,c=5)
+print()
+
+
 
 
 
