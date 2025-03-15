1499b07ec2cb3b1c964bb21b3a9bd94691c9ba16
Mark Peters <mapeters101@earthlink.net>
2025-03-15 16:20:05 -0400

    Work on lesson118 from 3:40 to 7:15.

--- lesson118.py
+++ lesson118.py
@@ -19,7 +19,10 @@ func(10, 20, c=50)
 func(c=1, b=2, a=3)
 print('-'*80)
 
+def func(a, b=2, c=3, *, d=10, e, f=30):
+    print(a, b, c, d, e, f)
 
-
-
+func(1, e=20)
+func(e=20, a=1)
+func(1, c=3.5, d=100, e=200)
 
