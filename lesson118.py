2b343977bac1c4dcecea539adb8ee53ba42d17d8
Mark Peters <mapeters101@earthlink.net>
2025-03-15 16:14:21 -0400

    Work on lesson118 from 2:20 to 3:40.

--- lesson118.py
+++ lesson118.py
@@ -4,6 +4,22 @@ def func(a, b, *args, c):
     print(args)
     print(c)
 
-print(func(10, 20, 30, 40, c=50))
-print(func(10, 20, 30, 40, 50))
+func(10, 20, 30, 40, c=50)
+#print(func(10, 20, 30, 40, 50))
+print('-'*80)
+
+
+def func(a, b, *, c):
+    print(a)
+    print(b)
+    print(c)
+
+func(10, 20, c=50)
+#print(func(10, 20, 30))
+func(c=1, b=2, a=3)
+print('-'*80)
+
+
+
+
 
