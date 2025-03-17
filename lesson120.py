c94ed16dddb51efbf74659a1c32eecf8c11fa824
Mark Peters <mapeters101@earthlink.net>
2025-03-17 13:38:42 -0400

    Work on lesson120 from 2:45 to 3:50.

---
+++ lesson120.py
@@ -0,0 +1,20 @@
+print(2 + 2)
+print(sum([1,2,3]) + max([0, -1, 1]))
+a = 2 + 2
+print(a)
+
+print(lambda a, b: a + b)
+print('-'*80)
+
+f = lambda a, b: a+b
+
+def add(a,b):
+    return a+b
+
+print(add)
+print(f)
+print()
+print(f(10, 20))
+
+
+
